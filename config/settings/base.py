d3dd7e5b1d87d4dd46337d523e1cd3d9cc0218c1
Marcin Szymanski <ms32035@gmail.com>
2020-01-27 16:30:30 +0000

    fix minor permission issues

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,7 +101,7 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "checks:datacheck_status"
+LOGIN_REDIRECT_URL = "users:redirect"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
